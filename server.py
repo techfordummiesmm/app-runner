10c5230b9dbafc67a4cf951c36d784e134492ded
Hlaing Min Paing <50479345+techfordummiesmm@users.noreply.github.com>
2023-12-03 00:18:36 +0630

    Update server.py

--- server.py
+++ server.py
@@ -7,7 +7,7 @@ def hello_world(request):
     name = os.environ.get('NAME')
     if name == None or len(name) == 0:
         name = "world"
-    message = "Hello, " + name + "!\n"
+    message = "Welcome from web app, " + name + "!\n"
     return Response(message)
 
 if __name__ == '__main__':
